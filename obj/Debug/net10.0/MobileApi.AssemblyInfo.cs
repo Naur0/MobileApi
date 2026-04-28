d3f7c95c0cf00451c4420c433670b44ac0c7a10a
alnur <alnur@email.com>
2026-04-28 23:37:04 +0800

    initial commit

--- obj/Debug/net10.0/MobileApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MobileApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+053ef72c7ce0e3a37d850d78e27e42180e1a6aff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
